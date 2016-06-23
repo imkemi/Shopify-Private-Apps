ae171765f9deb9188270c8f6b41644571761a204
imkemi <kamal.458769@gmail.com>
2016-06-23 17:27:44 +0530

    Add files via upload

---
+++ conf.php
@@ -0,0 +1,7 @@
+<?php
+
+	define('SHOPIFY_SHOP', 'your-store.myshopify.com');
+	define('SHOPIFY_APP_API_KEY', 'your-store.myshopifyapi-key');
+	define('SHOPIFY_APP_PASSWORD', 'your store shopify api password');
+
+?>
